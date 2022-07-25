cdb33a957f5ee2b62693853008cd9ccec2b65a2a
Felipe Cechin <ficechin@hotmail.com>
2022-07-25 14:58:46 -0300

    initial project concluded and finapi project started

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -2,8 +2,39 @@ const express = require('express')
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({ message: "Hello world" })
+app.use(express.json());
+
+app.get('/courses', (req, res) => {
+    return res.json([
+        "Curso 1", "Curso 2"
+    ])
+})
+
+app.post('/courses', (req, res) => {
+    console.log(req.body)
+    return res.json([
+        "Curso 1", "Curso 2", "Curso 3"
+    ])
+})
+
+app.put("/courses/:id", (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    return res.json([
+        "Curso 6", "Curso 2", "Curso 3"
+    ])
+})
+
+app.patch("/courses/:id", (req, res) => {
+    return res.json([
+        "Curso 6", "Curso 2", "Curso 3"
+    ])
+})
+
+app.delete("/courses/:id", (req, res) => {
+    return res.json([
+        "Curso 6", "Curso 2", "Curso 3"
+    ])
 })
 
 app.listen(3333);
