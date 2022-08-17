acf82ecaab98d59c728c6be326002be99d3d0aab
Felipe Cechin <ficechin@hotmail.com>
2022-08-17 17:51:26 -0300

    categories repository in memory created

--- rentalx/src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
+++ rentalx/src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -4,14 +4,26 @@ import {
   ICreateCategoryDTO,
 } from "../ICategoriesRepository";
 
-class CategoriesRepository implements ICategoriesRepository {
-  findByName(name: string): Promise<Category> {
-    throw new Error("Method not implemented.");
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = [];
+
+  async findByName(name: string): Promise<Category> {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
   }
-  list(): Promise<Category[]> {
-    throw new Error("Method not implemented.");
+
+  async list(): Promise<Category[]> {
+    const list = this.categories;
+    return list;
   }
-  create({ name, description }: ICreateCategoryDTO): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = new Category();
+
+    Object.assign(category, { name, description });
+
+    this.categories.push(category);
   }
 }
+
+export { CategoriesRepositoryInMemory };
