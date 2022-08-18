498012df600517a45ce801aff9b915615769de1d
Felipe Cechin <ficechin@hotmail.com>
2022-08-18 17:31:38 -0300

    refactoring project complete

---
+++ rentalx/src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,38 @@
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../infra/typeorm/entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+class UsersRepositoryInMemory implements IUsersRepository {
+  users: User[] = [];
+
+  updateUser(user: User): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.users.find((user) => user.email === email);
+  }
+
+  async create({
+    driver_license,
+    email,
+    name,
+    password,
+  }: ICreateUserDTO): Promise<void> {
+    const user = new User();
+
+    Object.assign(user, { driver_license, email, name, password });
+
+    this.users.push(user);
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.users.find((user) => user.id === id);
+  }
+
+  list(): Promise<User[]> {
+    throw new Error("Method not implemented.");
+  }
+}
+
+export { UsersRepositoryInMemory };
