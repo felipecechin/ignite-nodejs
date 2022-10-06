05711e8449174b680ca1642ec1672a69e30da2c7
Felipe Cechin Mello <ficechin@hotmail.com>
2022-10-06 15:43:54 -0300

    adjust to deploy

--- rentalx/src/shared/infra/typeorm/index.ts
+++ rentalx/src/shared/infra/typeorm/index.ts
@@ -10,11 +10,12 @@ import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.NODE_ENV === "test" ? "localhost" : "database_ignite",
+  host: process.env.NODE_ENV === "test" ? "localhost" : process.env.DB_HOST,
   port: 5432,
-  username: "docker",
-  password: "1234",
-  database: process.env.NODE_ENV === "test" ? "rentx_test" : "rentx",
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database:
+    process.env.NODE_ENV === "test" ? "rentx_test" : process.env.DB_NAME,
   entities: [Category, Specification, User, Car, CarImage, Rental, UserTokens],
   migrations: ["src/shared/infra/typeorm/migrations/*.ts"],
 });
